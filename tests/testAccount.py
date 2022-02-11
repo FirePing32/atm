19062569e5ec704f6f388772f6c00cd18fcaaa03
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-02-11 21:38:35 +0530

    add tests

--- tests/testAccount.py
+++ tests/testAccount.py
@@ -1,10 +1,10 @@
 import unittest
 import sys
-import csv
 
 sys.path.append('..')
 
 from utils.account import Account
+from unittest import mock
 
 accountObj = Account("aaabbb", 111111)
 
@@ -13,6 +13,15 @@ class TestAccount(unittest.TestCase):
     def test_balance(self):
         self.assertEqual(accountObj.getBalance(), "0")
 
+    def test_deposit(self):
+        with mock.patch('builtins.input', side_effect=[-10, -5, 0, 15]):
+            assert accountObj.depositMoney() == None
+        self.assertEqual(accountObj.getBalance(), "15")
+
+    def test_withdraw(self):
+        with mock.patch('builtins.input', side_effect=[-10, -5, 0, 20, 5]):
+            assert accountObj.withdrawMoney() == None
+        self.assertEqual(accountObj.getBalance(), "10")
 
 if __name__ == '__main__':
 	  unittest.main(verbosity=2)
